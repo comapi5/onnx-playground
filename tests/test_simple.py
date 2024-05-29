e6ffcfe532fccac1a490673d39809bd99c7c70c7
comapi <comapi0905@gmail.com>
2024-05-29 17:33:59 +0900

    Add: test_simple.py

---
+++ tests/test_simple.py
@@ -0,0 +1,26 @@
+import numpy as np
+import onnx
+import onnxruntime
+from onnx import TensorProto, helper
+
+
+def test_exp():
+    input = np.random.uniform(-1, 1, size=(3, 255, 255)).astype(np.float32)
+
+    graph = helper.make_graph(
+        nodes=[helper.make_node("Exp", ["input"], ["output"])],
+        name="test_exp",
+        inputs=[helper.make_tensor_value_info("input", TensorProto.FLOAT, input.shape)],
+        outputs=[
+            helper.make_tensor_value_info("output", TensorProto.FLOAT, input.shape)
+        ],
+    )
+
+    model = helper.make_model(graph)
+
+    session = onnxruntime.InferenceSession(model.SerializeToString())
+
+    ort_output = session.run(None, {"input": input})[0]
+    np_output = np.exp(input)
+
+    np.testing.assert_allclose(ort_output, np_output, rtol=2e-07)
