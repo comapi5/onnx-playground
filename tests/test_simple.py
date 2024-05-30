9a28b595b15ea8b6f78f7988b392d50798eab668
comapi <comapi0905@gmail.com>
2024-05-30 15:11:48 +0900

    Add: test_abs

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -4,6 +4,28 @@ import onnxruntime
 from onnx import TensorProto, helper
 
 
+def test_abs():
+    input = np.random.uniform(-100, 100, size=(3, 256, 256)).astype(np.float32)
+
+    graph = helper.make_graph(
+        nodes=[helper.make_node("Abs", ["input"], ["output"])],
+        name="test_abs",
+        inputs=[helper.make_tensor_value_info("input", TensorProto.FLOAT, input.shape)],
+        outputs=[
+            helper.make_tensor_value_info("output", TensorProto.FLOAT, input.shape)
+        ],
+    )
+
+    model = helper.make_model(graph)
+
+    session = onnxruntime.InferenceSession(model.SerializeToString())
+
+    ort_output = session.run(None, {"input": input})[0]
+    np_output = np.abs(input)
+
+    np.testing.assert_equal(ort_output, np_output)
+
+
 def test_exp():
     input = np.random.uniform(-1, 1, size=(3, 256, 256)).astype(np.float32)
 
