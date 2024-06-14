eaba28c8a58cfb6a6f5a901fcdad616aa9070843
comapi <comapi0905@gmail.com>
2024-06-14 18:09:23 +0900

    Add: test_celu

--- tests/test_element_wise_operator.py
+++ tests/test_element_wise_operator.py
@@ -12,12 +12,17 @@ def _test_element_wise_operator(
     input_shape=(3, 256, 256),
     input_min=-1.0,
     input_max=1.0,
+    attribute={},
     np_testing_function=np.testing.assert_equal,
 ):
     input = np.random.uniform(input_min, input_max, size=input_shape).astype(np.float32)
 
     graph = helper.make_graph(
-        nodes=[helper.make_node(operator_name.capitalize(), ["input"], ["output"])],
+        nodes=[
+            helper.make_node(
+                operator_name.capitalize(), ["input"], ["output"], **attribute
+            )
+        ],
         name=f"test_{operator_name.lower()}",
         inputs=[helper.make_tensor_value_info("input", TensorProto.FLOAT, input.shape)],
         outputs=[
@@ -30,7 +35,7 @@ def _test_element_wise_operator(
     session = onnxruntime.InferenceSession(model.SerializeToString())
 
     ort_output = session.run(None, {"input": input})[0]
-    np_output = np_func(input)
+    np_output = np_func(input, **attribute)
 
     np_testing_function(ort_output, np_output)
 
@@ -90,6 +95,18 @@ def test_ceil():
     _test_element_wise_operator(
         operator_name="Ceil",
         np_func=np.ceil,
+    )
+
+
+def test_celu():
+    def np_celu(x, alpha):
+        return np.where(x > 0, x, alpha * (np.exp(x / alpha) - 1))
+
+    _test_element_wise_operator(
+        operator_name="Celu",
+        attribute={"alpha": 1.0},
+        np_func=np_celu,
+        np_testing_function=functools.partial(np.testing.assert_allclose, rtol=1e-03),
     )
 
 
